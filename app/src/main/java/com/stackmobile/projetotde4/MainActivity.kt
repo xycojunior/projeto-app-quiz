82cf3b231f281141de1f235c7aa8febc04819f8c
Francisco José Júnior <fcojosejunior3103@gmail.com>
2024-05-28 12:04:07 -0300

    atualização + adição de condições

--- app/src/main/java/com/stackmobile/projetotde4/MainActivity.kt
+++ app/src/main/java/com/stackmobile/projetotde4/MainActivity.kt
@@ -10,22 +10,19 @@ import com.stackmobile.projetotde4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var currentQuestionIndex = 0
+    private var indexQuestao = 0
+    private var contadorRespostaCerta = 0
     private val questions = listOf(
-        Question("Qual das seguintes é uma linguagem de programação orientada a objetos?", listOf("HTML", "CSS", "PHP", "SQL"), 2),
-        Question(
-            "O que significa HTML?",
-            listOf("Hyper Text Markup Language", "Hyper Text Multiple Language", "Hyper Text Main Language", "High Tome Mala Layout"),
-            0
-        ),
-        Question("Qual dos seguintes não é um tipo de dado em Python?", listOf("int", "string", "float", "Real"), 3),
-        Question("Qual das opções a seguir é um loop em Python?", listOf("repeat", "while", "none", "until"), 1),
-        Question("Qual dos seguintes é um operador lógico em muitas linguagens de programação?", listOf("&&", "||", "!", "Todos os anteriores"), 3),
-        Question("Qual das seguintes linguagens é usada principalmente para desenvolvimento de aplicativos Android?", listOf("Ruby", "CSS", "Kotlin", "Swift"), 2),
-        Question("Qual das seguintes é uma biblioteca popular de JavaScript para construir interfaces de usuário?", listOf("React", "Laravel", "Django", "Flask"), 0),
-        Question("Qual das seguintes linguagens é conhecida por seu uso em ciência de dados e aprendizado de máquina?", listOf("PHP", "Python", "Ruby", "Java"), 1),
-        Question("Qual é a saída do seguinte código Python? print(\"Hello, World!\")", listOf(" Hello, World!", "Olá Mundo!", "Oi Mundo", "Helloworld"), 0),
-        Question("Qual é a função do comando print em Python?", listOf("Atribuir valores a variáveis", "Criar uma nova linha", "Imprimir valores na tela", "Verificar condições"), 2),
+        Question("1- Qual das seguintes é uma linguagem de programação orientada a objetos?", listOf("HTML", "CSS", "PHP", "SQL"), 2),
+        Question("2- O que significa HTML?", listOf("Hyper Text Markup Language", "Hyper Text Multiple Language", "Hyper Text Main Language", "High Tome Mala Layout"), 0),
+        Question("3- Qual dos seguintes não é um tipo de dado em Python?", listOf("int", "string", "float", "Real"), 3),
+        Question("4- Qual das opções a seguir é um loop em Python?", listOf("repeat", "while", "none", "until"), 1),
+        Question("5- Qual dos seguintes é um operador lógico em muitas linguagens de programação?", listOf("&&", "||", "!", "Todos os anteriores"), 3),
+        Question("6- Qual das seguintes linguagens é usada principalmente para desenvolvimento de aplicativos Android?", listOf("Ruby", "CSS", "Kotlin", "Swift"), 2),
+        Question("7- Qual das seguintes é uma biblioteca popular de JavaScript para construir interfaces de usuário?", listOf("React", "Laravel", "Django", "Flask"), 0),
+        Question("8- Qual das seguintes linguagens é conhecida por seu uso em ciência de dados e aprendizado de máquina?", listOf("PHP", "Python", "Ruby", "Java"), 1),
+        Question("9- Qual é a saída do seguinte código Python? print(\"Hello, World!\")", listOf(" Hello, World!", "Olá Mundo!", "Oi Mundo", "Helloworld"), 0),
+        Question("10- Qual é a função do comando print em Python?", listOf("Atribuir valores a variáveis", "Criar uma nova linha", "Imprimir valores na tela", "Verificar condições"), 2),
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +36,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun carregarQuestao() {
-        val questao = questions[currentQuestionIndex]
+        val questao = questions[indexQuestao]
         binding.questionTextView.text = questao.question
         binding.opcoesRadio.removeAllViews()
         questao.options.forEachIndexed { index, option ->
@@ -52,28 +49,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkResposta() {
-        val selectedOptionId = binding.opcoesRadio.checkedRadioButtonId
-        if (selectedOptionId != -1) {
-            val selectedIndex = binding.opcoesRadio.indexOfChild(findViewById(selectedOptionId))
-            val question = questions[currentQuestionIndex]
-            if (selectedIndex == question.correctAnswerIndex) {
+        val selecioneOpcaoId = binding.opcoesRadio.checkedRadioButtonId
+        if (selecioneOpcaoId != -1) {
+            val selecioneIndice = binding.opcoesRadio.indexOfChild(findViewById(selecioneOpcaoId))
+            val question = questions[indexQuestao]
+            if (selecioneIndice == question.correctAnswerIndex) {
+                contadorRespostaCerta++
                 binding.feedbackTextView.text = "Correto!"
             } else {
-                binding.feedbackTextView.text =
-                    "Errado! A resposta correta é: ${question.options[question.correctAnswerIndex]}"
+                binding.feedbackTextView.text = "Errado! A resposta correta é: ${question.options[question.correctAnswerIndex]}"
             }
             binding.feedbackTextView.visibility = View.VISIBLE
 
             // Atraso antes de carregar a próxima pergunta
             Handler(Looper.getMainLooper()).postDelayed({
-                currentQuestionIndex++
-                if (currentQuestionIndex < questions.size) {
+                indexQuestao++
+                if (indexQuestao < questions.size) {
                     carregarQuestao()
                 } else {
-                    binding.botaoEnviar.isEnabled = false
-                    binding.feedbackTextView.text = "Quiz concluído!"
+                    mostrarResultado()
                 }
-            }, 2500)
+            }, 2200)
         }
+    }
+
+    private fun mostrarResultado() {
+        val totalQuestions = questions.size
+        val porcentagem = (contadorRespostaCerta * 100) / totalQuestions
+
+        val message = when {
+            porcentagem < 50 -> "Poxa você não foi muito bem. Mais sorte na próxima tentativa."
+            porcentagem == 50 -> "Muito bem, você está no caminho certo, vamos melhorar."
+            else -> "Uau, você é espetacular. Me curvo a sua sabedoria."
+        }
+
+        binding.questionTextView.text = "Quiz concluído! Você acertou $contadorRespostaCerta de $totalQuestions perguntas. $message"
+        binding.opcoesRadio.removeAllViews()
+        binding.botaoEnviar.isEnabled = false
     }
 }
